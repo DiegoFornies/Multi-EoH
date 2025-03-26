ee5b4655395d1cb7689351b3d20f227610a53f3c
DiegoFornies <123958878+DiegoFornies@users.noreply.github.com>
2025-03-26 15:48:00 +0100

    Update llm_client.py

--- llm/llm_client.py
+++ llm/llm_client.py
@@ -3,7 +3,7 @@ import time
 
 class LLMClient:
     def __init__(self):
-        genai.configure(api_key='AIzaSyDwCd7UCMxYe7zQKlho91CHBRZLc8SHdQ0')
+        genai.configure(api_key='')
         self.model = genai.GenerativeModel('gemini-2.0-flash')
         self.total_petitions = 0
         self.counter = 0
